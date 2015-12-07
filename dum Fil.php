fd8de949b127884abc36622c1941f7418b334ce8
erieks862 <erieks862@E29612.edu.linkoping.se>
2015-12-07 14:59:39 +0100

    7/12 15:00
    html grund, struktur för chaten klar

---
+++ dum Fil.php
@@ -0,0 +1,9 @@
+
+ett meddelande
+<div class="message">
+    <h1>NAMN</h1><h2>DATUM</h2>
+    <hr>
+    <p></p>
+    <div class="answers">(nya svar här!)</div>
+    <a class="svara">Svara</a>
+</div>
--- index.php
+++ index.php
@@ -1,8 +1,77 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <link rel="stylesheet" type="text/css" href="index.css">
+        <title>Title</title>
     </head>
     <body>
+        <div id="header">
+            hej
+        </div>
+        <div id="content">
+            <div id="mainWindow">
+                <div class="message">
+                    <h1>Ett Namn</h1><h2> 2015-12-12 12:12</h2>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Praesent ex diam, pharetra id efficitur gravida, vehicula a elit. </p>
+                    <div class="answers">
+                        <div class="message">
+                            <h1>Ett Namn</h1><h2> 2015-12-12 12:12</h2>
+                            <hr>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Praesent ex diam, pharetra id efficitur gravida, vehicula a elit. </p>
+                            <div class="answers">
+                                <div class="message">
+                                    <h1>Ett Namn</h1><h2> 2015-12-12 12:12</h2>
+                                    <hr>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Praesent ex diam, pharetra id efficitur gravida, vehicula a elit. </p>
+                                    <div class="answers">
+                                        <div class="message">
+                                            <h1>Ett Namn</h1><h2> 2015-12-12 12:12</h2>
+                                            <hr>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Praesent ex diam, pharetra id efficitur gravida, vehicula a elit. </p>
+                                            <div class="answers"></div>
+                                            <a class="svara">Svara</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="message">
+                                    <h1>Ett Namn</h1><h2> 2015-12-12 12:12</h2>
+                                    <hr>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Praesent ex diam, pharetra id efficitur gravida, vehicula a elit. </p>
+                                    <div class="answers"></div>
+                                    <a class="svara">Svara</a>
+                                </div>
+                            </div>
+                            <a class="svara">Svara</a>
+                        </div>
+                    </div>
+                    <a class="svara">Svara</a>
+                </div>
+                <div class="message">
+                    <h1>Ett Namn</h1><h2> 2015-12-12 12:12</h2>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Praesent ex diam, pharetra id efficitur gravida, vehicula a elit. </p>
+                    <div class="answer"></div>
+                    <a class="svara">Svara</a>
+                </div>
+            </div>
+            <div id="sidePanel">
+                <h3>Trådar</h3>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>5</li>
+                    <li>6</li>
+                    <li>7</li>
+                    <li>8</li>
+                    <li>9</li>
+                    <li>0</li>
+                    <li>10</li>
+                </ul>
+            </div>
+        </div>
     </body>
 </html>
